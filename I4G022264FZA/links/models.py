f6d1f0ab46cfcd06a93ff808d1b0e0dda96c09f8
PatrickNaenae <naenaepatrick@gmail.com>
2022-06-30 15:29:57 +0100

    added the django link app

---
+++ I4G022264FZA/links/models.py
@@ -0,0 +1,15 @@
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Link(models.Model):
+    target_url = models.URLField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True)
+    identifier = models.SlugField(blank=True, unique=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    created_date = models.DateTimeField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.target_url
